e7ad7985d2cdff178411403ef426664145403a05
quyenngo21 <quyenwat@gmail.com>
2024-05-28 22:25:47 +0700

    fixed

---
+++ FE/FootBall/N.Service/Constant/BookingStatusConstant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N.Service.Constant
+{
+    public class BookingStatusConstant
+    {
+        public static string Confirm => "Confirm";
+        public static string Wait => "Wait";
+        public static string Cancel => "Cancel";
+    }
+}
